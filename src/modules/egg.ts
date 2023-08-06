de8c56b90630c2157088bc51cdc54857f1f300bb
agnbart <polpumi@gmail.com>
2023-08-06 10:37:50 +0200

    second init game

---
+++ src/modules/egg.ts
@@ -0,0 +1,12 @@
+export enum EggState {
+    Egg = "egg",
+    Tamagotchi = "tamagotchi",
+}
+
+export class Egg {
+    eggClicks: number = 0;
+    assets = new Map([
+        [EggState.Egg, "assets/egg.svg"],
+        [EggState.Tamagotchi, "assets/tamagotchi.svg"],
+    ]);
+}
