5e57e2775f2a91a12fe2c2b7f3162b33a3c5f66d
agnbart <polpumi@gmail.com>
2023-08-24 12:13:32 +0200

    feat: reset button

--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,12 @@ document.addEventListener("DOMContentLoaded", function () {
     ) as HTMLParagraphElement | null;
     const egg = document.querySelector("#egg") as HTMLImageElement | null;
     const result = document.querySelector("#result") as HTMLImageElement | null;
+    const actionButton = document.querySelector("#action-btn") as HTMLButtonElement | null;
 
     const game = new Game();
+
     game.init({
+        actionButtonElement: actionButton,
         resultElement: result,
         counterElement: counter,
         eggElement: egg,
